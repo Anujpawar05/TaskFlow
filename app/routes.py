fc37ff00300ab8dc1f0b484469308ee12a806ee1
Anujpawar05 <anujpawar025@gmail.com>
2026-07-01 19:58:32 +0530

    Implement delete task functionality

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,42 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+
+from .models import db, Task
 
 main = Blueprint("main", __name__)
 
 
-@main.route("/")
+@main.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    if request.method == "POST":
+
+        title = request.form["title"]
+
+        description = request.form["description"]
+
+        new_task = Task(
+            title=title,
+            description=description
+        )
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        return redirect("/")
+
+    tasks = Task.query.all()
+
+    return render_template(
+        "index.html",
+        tasks=tasks
+    )
+@main.route("/delete/<int:id>")
+def delete_task(id):
+
+    task = Task.query.get_or_404(id)
+
+    db.session.delete(task)
+
+    db.session.commit()
+
+    return redirect("/")
