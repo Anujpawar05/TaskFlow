bcd16027621153bfb19749d89678ebd708ebab4f
Anujpawar05 <anujpawar025@gmail.com>
2026-07-01 20:04:52 +0530

    Implement complete CRUD functionality

--- app/routes.py
+++ app/routes.py
@@ -40,3 +40,23 @@ def delete_task(id):
     db.session.commit()
 
     return redirect("/")
+
+@main.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_task(id):
+
+    task = Task.query.get_or_404(id)
+
+    if request.method == "POST":
+
+        task.title = request.form["title"]
+
+        task.description = request.form["description"]
+
+        db.session.commit()
+
+        return redirect("/")
+
+    return render_template(
+        "edit.html",
+        task=task
+    )
